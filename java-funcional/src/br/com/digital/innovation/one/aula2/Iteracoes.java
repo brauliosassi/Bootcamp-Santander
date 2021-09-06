6df69c2372c22933b7b763482977e616964b8cae
brauliosassi <braulio.sassi@gmail.com>
2021-09-06 20:03:19 -0300

    Aula 2

--- java-funcional/src/br/com/digital/innovation/one/aula2/Iteracoes.java
+++ java-funcional/src/br/com/digital/innovation/one/aula2/Iteracoes.java
@@ -1,14 +1,27 @@
 package br.com.digital.innovation.one.aula2;
 
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class Iteracoes  {
     public static void main(String[] args) {
         String[] nomes = {"João", "Sássi", "Braulio", "Java"};
-        Integer[] numeros = {1,2,3,4};
+        Integer[] numeros = {1,2,3,4,5};
 //        imprimirNomesFiltrados(nomes);
-        imprimirTodosNomes(nomes);
+//        imprimirTodosNomes(nomes);
+//        imprimiODobroDeCadaItemDaLista(numeros);
+
+
+    List<String> profissoes = new ArrayList<>();
+    profissoes.add("Desenvolvedor");
+    profissoes.add("Testador");
+    profissoes.add("Gerente de Projeto");
+    profissoes.add("Gerente de Qualidade");
+
+    profissoes.stream()
+            .filter( profissao -> profissao.startsWith("Gerente"))
+            .forEach(System.out::println);
     }
     /**
     private static void imprimirNomesFiltrados(String... nomes) {
@@ -33,16 +46,18 @@ public class Iteracoes  {
     System.out.println(nomesResultados);
      */
 
-    public static void imprimirTodosNomes(String... nomes){
-        for(String nome : nomes) {
-            System.out.println("Imprimido pelo for: "+nome);
-        }
-        Stream.of(nomes)
-                .forEach(nome -> System.out.println("Imprimido pelo forEach: "+nome));
-    }
-    public static void imprimiODobroDeCadaItemDaLista(Integer... numeros){
-        for (Integer numero: numeros) {
-            System.out.println(numero*2);
-        }
-    }
+//    public static void imprimirTodosNomes(String... nomes){
+//        for(String nome : nomes) {
+//            System.out.println("Imprimido pelo for: "+nome);
+//        }
+//        Stream.of(nomes)
+//                .forEach(nome -> System.out.println("Imprimido pelo forEach: "+nome));
+//    }
+//    public static void imprimiODobroDeCadaItemDaLista(Integer... numeros){
+//        for (Integer numero: numeros) {
+//            System.out.println("imprimindo pelo for: "+numero*2);
+//        }
+//        Stream.of(numeros).map( numero -> numero*2)
+//                          .forEach(System.out::println);
+//    }
 }
