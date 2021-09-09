5a7c658f90df1a37a89325f50641e376b96968b9
brauliosassi <braulio.sassi@gmail.com>
2021-09-09 19:42:18 -0300

    Projeto com Java modular

--- java-funcional/src/br/com/digital/innovation/one/aula3/PararelStreamExemplo.java
+++ java-funcional/src/br/com/digital/innovation/one/aula3/PararelStreamExemplo.java
@@ -1,6 +1,37 @@
 package br.com.digital.innovation.one.aula3;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static br.com.digital.innovation.one.aula1.FatorialRecursivo.FatorialRecurssivo.fatorial;
+
 public class PararelStreamExemplo {
 
+    public static long main(String[] args) {
+//        long inicio = System.currentTimeMillis();
+//        IntStream.range(1, 100000).forEach(num -> fatorial(num));
+//        long fim = System.currentTimeMillis();
+//
+//        System.out.println("tempo de execução serial :: "+(fim - inicio));
+//
+//         inicio = System.currentTimeMillis();
+//        IntStream.range(1, 100000).forEach(num -> fatorial(num));
+//         fim = System.currentTimeMillis();
+//
+//        System.out.println("tempo de execução paralela :: "+(fim - inicio));
 
+        List<String> nomes = Arrays.asList("João", "Lula", "Bolsonaro", "Sérgio Mouro", "Trump");
+        nomes.parallelStream().forEach(System.out::println);
+
+//        public static long fatorial(long num){
+//              fat = 1;
+//
+//            for (long i = 2; i <= num; i++) {
+//                fat *= i;
+//            }
+//            return fat;
+//        }
+        return 0;
+    }
 }
---
+++ java-modular/src/digital/innovavtion/one/Main.java
@@ -0,0 +1,8 @@
+package digital.innovavtion.one;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
